7c094759aad5c3cd0faac511b5d9e4005e3834fb
mari4e4ka <126088784+mari4e4ka@users.noreply.github.com>
2023-06-18 12:48:26 +0200

    Update script-1

--- script-1.js
+++ script-1.js
@@ -1,24 +1,68 @@
-const friends = ["One", "Two", "Three", "One", "Two", "Three"];
+// const friends = ["One", "Two", "Three", "One", "Two", "Three"];
 
-console.log(friends);
+// console.log(friends);
 
-const lastIndex = friends.length - 1;
+// const lastIndex = friends.length - 1;
 
-// console.log(lastIndex);
+// // console.log(lastIndex);
 
-// console.log(friends[lastIndex]);
+// // console.log(friends[lastIndex]);
 
-friends[lastIndex] = "Ten";
-friends[3] = "four";
-friends[4] = "five";
+// friends[lastIndex] = "Ten";
+// friends[3] = "four";
+// friends[4] = "five";
 
-// console.table(friends);
+// // console.table(friends);
 
-// for (let i =  0; i < friends.length; i += 1) {
-//     console.log(friends[i]);
+// // for (let i =  0; i < friends.length; i += 1) {
+// //     console.log(friends[i]);
+// // };
+
+// for (const friend of friends) {
+//     console.log(friend);
+// }
+
+// const prise = [0, 1, 3, 10, 5];
+
+// let totalPrise = 0;
+
+// for (let i = 0; i < prise.length; i += 1) {
+//   if (prise[i] % 2 === 0) {
+//     totalPrise += prise[i];
+//   }
+// }
+
+// for (let value of prise) {
+//   if (value % 2 === 0) {
+//     totalPrise += value;
+//   }
+// }
+
+// for (let value of prise) {
+//   if (value % 2 !== 0) {
+//     continue;
+//   }
+//   totalPrise += value;
+// }
+
+// console.log(`Total Prise: ${totalPrise}$`);
+
+// const prise = [0, 1, 3, 10, 5];
+// const uzer = 11;
+// let message=`Користувач ${uzer} не знайдений`;
+// if (prise.includes(uzer)) {
+//   message = `Користувач ${uzer} знайдений`;
 // };
 
-for (const friend of friends) {
-    console.log(friend);
-}
+// console.log(message);
+//метод інклуд!!! для перевірки чи є певний елемент у масиві!!!
 
+// !!!!!!! ТЕРЕНАРНИК !!!!!!!!!!!!!!
+const uzers = [0, 1, 3, 10, 5];
+const uzer = 11;
+
+const message = uzers.includes(uzer)
+  ? `Користувач ${uzer} знайдений`
+  : `Користувач ${uzer} не знайдений`;
+
+console.log(message);
