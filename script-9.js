16c75708e4f5e34704518d155e47f4cf59c0c249
mari4e4ka <126088784+mari4e4ka@users.noreply.github.com>
2023-10-03 11:02:23 +0200

    update

--- script-9.js
+++ script-9.js
@@ -26,10 +26,47 @@
 // }
 // console.log (b*2);
 
-let link = "https://meta.ua/mail";
+// let link = "https://meta.ua/mail";
 
-if (!link.endsWith("/")) {
-  link += "/";
+// if (!link.endsWith("/")) {
+//   link += "/";
+// }
+
+// console.log(link);
+
+// let link = "https://my-site.com/about";
+
+// if (!link.endsWith("/") && link.includes('my-site')) {
+//   link += "/";
+// }
+
+// console.log(link);
+
+// let link = "https://my-site.com/about";
+
+// !link.endsWith("/") && link.includes('my-site') ? console.log(link += "/") : console.log(link);
+
+// console.log(!link.endsWith("/") && link.includes('my-site') ? link += "/" : link);
+
+const min = 24;
+const max = 76;
+console.log (min, max);
+
+for (let i = min; i<= max; i += 1) {
+  if (!(i % 5)) {
+    console.log(i);
+  }
 }
 
-console.log(link);
+// const logIn = prompt("Ви хто?");
+
+// if (logIn === "адмін") {
+//   const pasword = prompt("Введіть пароль");
+//   pasword === "я адмін"
+//     ? console.log("Привіт!")
+//     : console.log("Невірний пароль!");
+// } else if (!logIn) {
+//   console.log("Скасовано");
+// } else {
+//   console.log("Я вас не знаю");
+// }
