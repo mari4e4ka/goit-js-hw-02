8a0b0c85d82fa254e91df46059fa0f7b8664fa7d
mari4e4ka <126088784+mari4e4ka@users.noreply.github.com>
2023-10-14 11:02:13 +0200

    update script-10

--- script-10-objects.js
+++ script-10-objects.js
@@ -27,23 +27,84 @@
 
 // friendsNames(friends);
 
-const friends = [
-  { name: "Enn", online: false },
-  { name: "Bob", online: true },
-  { name: "Mery", online: false },
+// const friends = [
+//   { name: "Enn", online: false },
+//   { name: "Bob", online: true },
+//   { name: "Mery", online: false },
+// ];
+
+// function isOnline(arr) {
+//   const isOnlineList = [];
+
+//   for (const arrg of arr) {
+//     if (arrg.online){
+//           isOnlineList.push(arrg);
+//     }
+
+//   }
+
+//   console.log(isOnlineList);
+// }
+
+// isOnline(friends);
+
+// const user = {
+//   name: "Alex",
+//   hobby: "html",
+// };
+
+// user.mood = "happy";
+// user.hobby = "skydiving";
+// console.log(user);
+
+// const keys = Object.keys(user);
+// console.log(keys);
+
+// for (const key of keys) {
+//   console.log(`${key}: ${user[key]}`);
+// }
+
+// for (const key in user) {
+//  console.log(key,': ', user[key]);
+// }
+
+// const salaries = {
+//   worker1: 100,
+//   worker2: 160,
+//   worker3: 130,
+// };
+
+// let sum = 0;
+// const salariesList = Object.values(salaries);
+// for(const value of salariesList){
+//   sum+=value;
+// }
+// console.log(`Сума зарплат за поточний місяць = ${sum}$`);
+
+// for (const key in salaries) {
+//   sum += salaries[key];
+// }
+// console.log(sum);
+
+const stone = [
+  {
+    name: "Gold",
+    price: 1300,
+    quantity: 2,
+  },
+  {
+    name: "Silver",
+    price: 300,
+    quantity: 10,
+  },
 ];
 
-function isOnline(arr) {
-  const isOnlineList = [];
+function calcTotalPrice (stone){
 
-  for (const arrg of arr) {
-    if (arrg.online){
-          isOnlineList.push(arrg);  
-    }
-
+  for(let i=0; i<stone.lenght; i+=1){
+    const totalPrice = stone[i].price*stone[i].quantity;
+  console.log(`Загальна вартість = ${totalPrice}` )
   }
-
-  console.log(isOnlineList);
 }
 
-isOnline(friends);
+calcTotalPrice (stone);
