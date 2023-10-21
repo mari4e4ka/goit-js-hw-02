439703d735fdac120e5b5086897bbb310890965f
mari4e4ka <126088784+mari4e4ka@users.noreply.github.com>
2023-10-21 12:59:56 +0200

    update module-3.js

--- module-3.js
+++ module-3.js
@@ -1,16 +1,28 @@
-const arr = [2, 111, 1, 3, 4, 555];
-const [first, ...restArr] = arr;
-console.log(first);
-console.log(restArr);
+// const arr = [2, 111, 1, 3, 4, 555];
+// const [first, ...restArr] = arr;
+// console.log(first);
+// console.log(restArr);
 
-const newArr = [];
-for (const item of restArr) {
-    newArr.push(item*first);
+// const newArr = [];
+// for (const item of restArr) {
+//     newArr.push(item*first);
+// }
+// console.log(newArr);
+
+// const max = Math.max(...arr);
+// const min = Math.min(...arr);
+// console.log(max);
+// console.log(min);
+
+function squareDigits(num) {
+  const x = [...("" + num)];
+  let y = [];
+  for (const number of x) {
+    y.push(number * number);
+  }
+  return Number(y.join(""));
 }
-console.log(newArr);
 
-const max = Math.max(...arr);
-const min = Math.min(...arr);
-console.log(max);
-console.log(min);
-
+console.log(squareDigits(123));
+console.log(squareDigits(0));
+console.log(squareDigits(9119));
