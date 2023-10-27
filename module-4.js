23b163bd88a62181c078ea27daffeec5b19672a9
mari4e4ka <126088784+mari4e4ka@users.noreply.github.com>
2023-10-27 13:52:54 +0200

    update module-4.js

--- module-4.js
+++ module-4.js
@@ -150,7 +150,6 @@
 //   return item1;
 // }
 
-
 // const testArr = [1, 2, 3, 4, 5];
 
 // console.log(nextInLine(testArr, 10));
@@ -158,3 +157,74 @@
 // console.log(testArr);
 // console.log(testArr[4]);
 
+// function caseInSwitch(val) {
+//   let answer = "";
+//   switch (val) {
+//     case 1:
+//       answer = "alpha";
+//       break;
+//     case 2:
+//       answer = "beta";
+//       break;
+//     case 3:
+//       answer = "gamma";
+//       break;
+//     case 4:
+//       answer = "delta";
+//       break;
+//   }
+
+//   return answer;
+// }
+// console.log(caseInSwitch(3));
+
+// function caseInSwitch(val) {
+//   let answer = "";
+//   switch (val) {
+//     case "a":
+//       answer = "apple";
+//       break;
+//     case "b":
+//       answer = "bird";
+//       break;
+//     case "c":
+//       answer = "cat";
+//       break;
+//     default:
+//         answer ="stuff";
+//       break;
+//   }
+
+//   return answer;
+// }
+// console.log(caseInSwitch("h"));
+
+function sequentialSizes(val) {
+  let answer = "";
+  switch (val) {
+    case 1:
+    case 2:
+    case 3:
+      answer = "Low";
+      break;
+    case 4:
+    case 5:
+    case 6:
+      answer = "Mid";
+      break;
+    case 7:
+    case 8:
+    case 9:
+      answer = "High";
+      break;
+    default:
+      answer = "uncorect number";
+  }
+
+  // Змініть код лише над цим рядком
+  return answer;
+}
+
+sequentialSizes(8);
+
+console.log(sequentialSizes(2));
