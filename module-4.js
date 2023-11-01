25c05140740f4a79e52ded02e38e242fef1cf62c
mari4e4ka <126088784+mari4e4ka@users.noreply.github.com>
2023-11-01 14:29:21 +0100

    add map, filter, find, some, every, reduce method

--- module-4.js
+++ module-4.js
@@ -471,61 +471,302 @@
 //   }
 // }
 
-
 // console.log(rangeOfNumbers(1,5));
-
 
 // function rangeOfNumbers(startNum, endNum) {
 //   return startNum>endNum? []:[startNum,...rangeOfNumbers(startNum+1,endNum)]
 //   }
-  
-  
+
 //   console.log(rangeOfNumbers(2,10));
-
-
 
 // Перебираючі методи масивів
 
-const contacts = [
-  {
-    firstName: "Akira",
-    lastName: "Laine",
-    number: "0543236543",
-    likes: ["Pizza", "Coding", "Brownie Points"],
-  },
-  {
-    firstName: "Harry",
-    lastName: "Potter",
-    number: "0994372684",
-    likes: ["Hogwarts", "Magic", "Hagrid"],
-  },
-  {
-    firstName: "Sherlock",
-    lastName: "Holmes",
-    number: "0487345643",
-    likes: ["Intriguing Cases", "Violin"],
-  },
-  {
-    firstName: "Kristian",
-    lastName: "Vos",
-    number: "unknown",
-    likes: ["JavaScript", "Gaming", "Foxes"],
-  },
-];
+// const contacts = [
+//   {
+//     firstName: "Akira",
+//     lastName: "Laine",
+//     number: "0543236543",
+//     likes: ["Pizza", "Coding", "Brownie Points"],
+//   },
+//   {
+//     firstName: "Harry",
+//     lastName: "Potter",
+//     number: "0994372684",
+//     likes: ["Hogwarts", "Magic", "Hagrid"],
+//   },
+//   {
+//     firstName: "Sherlock",
+//     lastName: "Holmes",
+//     number: "0487345643",
+//     likes: ["Intriguing Cases", "Violin"],
+//   },
+//   {
+//     firstName: "Kristian",
+//     lastName: "Vos",
+//     number: "unknown",
+//     likes: ["JavaScript", "Gaming", "Foxes"],
+//   },
+// ];
 
-const contactNames = contacts.map(contact=>contact.firstName);
-console.log(contactNames);
-const contactLikes = contacts.flatMap(contact=>contact.likes);
-console.log(contactLikes);
+// const contactNames = contacts.map(contact=>contact.firstName);
+// console.log(contactNames);
+// const contactLikes = contacts.flatMap(contact=>contact.likes);
+// console.log(contactLikes);
 
-const newContacts = contacts.map((contact, idx)=>({...contact, likes: [...contact.likes, "sing"], firstName: `${contact.firstName} ${((idx)+1)}`}));
-console.log(newContacts);
+// const newContacts = contacts.map((contact, idx)=>({...contact, likes: [...contact.likes, "sing"], firstName: `${contact.firstName} ${((idx)+1)}`}));
+// console.log(newContacts);
 
-const newContact = contacts.map((contact, idx)=>{
-    if (contact.firstName==="Harry"){
-    
-        contact.number= "1";
-    }
-    return contact
-});
-console.log(newContact);
+// const newContact = contacts.map((contact, idx)=>{
+//     if (contact.firstName==="Harry"){
+
+//         contact.number= "1";
+//     }
+//     return contact
+// });
+// console.log(newContact);
+
+// const contacts = [
+//   {
+//     firstName: "Akira",
+//     lastName: "Laine",
+//     number: "0543236543",
+//     likes: ["Pizza", "Coding", "Brownie Points"],
+//   },
+//   {
+//     firstName: "Harry",
+//     lastName: "Potter",
+//     number: "0994372684",
+//     likes: ["Hogwarts", "Magic", "Hagrid"],
+//   },
+//   {
+//     firstName: "Sherlock",
+//     lastName: "Holmes",
+//     number: "0487345643",
+//     likes: ["Intriguing Cases", "Violin"],
+//   },
+//   {
+//     firstName: "Kristian",
+//     lastName: "Vos",
+//     number: "unknown",
+//     likes: ["JavaScript", "Gaming", "Foxes"],
+//   },
+// ];
+// console.table(contacts);
+
+// const updateContactsLikes = contacts.map((contact) => {
+//   return contact.firstName === "Harry"
+//     ? { ...contact, likes: [...contact.likes, "sing"] }
+//     : contact;
+// });
+
+// console.table(updateContactsLikes);
+
+// const updateContactsNumber = contacts.map((contact) =>
+//   contact.firstName === "Harry"
+//     ? { ...contact, number: contact.number + 1111 }
+//     : contact
+// );
+
+// console.table(updateContactsNumber);
+
+// console.log([1,2,3,4,5,6,7,8,9,10].map(num=>num*10));
+
+// const up10=[1,2,3,4,5,6,7,8,9,10].map(num=>num*10);
+// console.log(up10);
+
+// const arr1=[1,2,3,4,5,6,7,8,9,10];
+// const arr2 =[2,2,2,2,2,2,2,2,2,2];
+
+// const increaseByANumber=(arr,num)=>arr.map(item=>item*num);
+
+// console.log(increaseByANumber(arr1,1));
+// console.log(increaseByANumber(arr1,2));
+// console.log(increaseByANumber(arr1,10));
+// console.log(increaseByANumber(arr1,100));
+
+// console.log(increaseByANumber(arr2,1));
+// console.log(increaseByANumber(arr2,2));
+// console.log(increaseByANumber(arr2,10));
+// console.log(increaseByANumber(arr2,100));
+
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const bigNum = arr.filter((number) => number > 5);
+// console.log(bigNum);
+
+// console.table(contacts);
+
+// const contactsIsOnline = contacts.filter((contact) => contact.isOnline);
+// console.table(contactsIsOnline);
+
+// const contactsIsOffline = contacts.filter((contact) => !contact.isOnline);
+// console.table(contactsIsOffline);
+
+// console.table(contacts);
+
+// const contactsIsOnline = contacts.filter(({isOnline}) => isOnline);
+// console.table(contactsIsOnline);
+
+// const contactsIsOffline = contacts.filter(({isOnline}) => !isOnline);
+// console.table(contactsIsOffline);
+
+// const contacts = [
+//   {
+//     firstName: "Akira",
+//     lastName: "Laine",
+//     number: "0543236543",
+//     likes: ["Pizza", "Coding", "Brownie Points"],
+//     isOnline: true,
+//   },
+//   {
+//     firstName: "Harry",
+//     lastName: "Potter",
+//     number: "0994372684",
+//     likes: ["Hogwarts", "Magic", "Hagrid"],
+//     isOnline: false,
+//   },
+// ];
+
+// // const potterContact = contacts.find(({lastName})=>lastName==="Potter");
+// // console.log(potterContact);
+
+// const findContactByLastName = (contacts, contactToFind) =>
+//   console.log(contacts.find(({lastName}) => lastName === contactToFind));
+
+// findContactByLastName(contacts, "Potter");
+// findContactByLastName(contacts, "Laine");
+// findContactByLastName(contacts, "Piter");
+
+// const arr1=[1,2,3,4,5,6,7,8,9,10];
+// const arr2 =[2,2,2,2,2,2,2,2,2,2];
+
+// // const total1 = arr1.reduce((acc,num)=>acc+=num,0);
+// // console.log(total1);
+
+// // const total2 = arr2.reduce((acc,num)=>acc+=num,0);
+// // console.log(total2);
+
+// const total = arr => console.log(arr.reduce((acc,num)=>acc+num));
+
+// total(arr1);
+// total(arr2);
+
+// const salary = {
+//   1:100,
+//   2:200,
+//   3:300,
+// }
+
+// // const objectValues = Object.values(salary);
+// // console.log(objectValues);
+
+// const totalSalary = Object.values(salary).reduce((total,salary)=>acc+salary,0);
+
+// console.log(totalSalary);
+
+// const contacts = [
+//   {
+//     firstName: "Akira",
+//     lastName: "Laine",
+//     number: 230,
+//     likes: ["Pizza", "Coding", "Brownie Points"],
+//     isOnline: true,
+//   },
+//   {
+//     firstName: "Harry",
+//     lastName: "Potter",
+//     number: 150,
+//     likes: ["Hogwarts", "Magic", "Hagrid"],
+//     isOnline: false,
+//   },
+//   {
+//     firstName: "Akira",
+//     lastName: "Laine",
+//     number: 230,
+//     likes: ["Pizza", "Coding", "Brownie Points"],
+//     isOnline: true,
+//   },
+//   {
+//     firstName: "Harry",
+//     lastName: "Potter",
+//     number: 150,
+//     likes: ["Hogwarts", "Magic", "Hagrid"],
+//     isOnline: false,
+//   },
+//   {
+//     firstName: "Akira",
+//     lastName: "Laine",
+//     number: 230,
+//     likes: ["Pizza", "Coding", "Brownie Points"],
+//     isOnline: true,
+//   },
+//   {
+//     firstName: "Harry",
+//     lastName: "Potter",
+//     number: 150,
+//     likes: ["Hogwarts", "Magic", "Hagrid"],
+//     isOnline: false,
+//   },
+// ];
+
+// const totalNumber = contacts.reduce(
+//   (total, { number }) => total + number,
+//   0
+// );
+
+// console.log(`Загальний номер: ${totalNumber}`);
+
+// const fruits = [
+//   {
+//     name: "apple",
+//     quantity: 10,
+//     price: 10,
+//     isFresh: false,
+//   },
+//   {
+//     name: "pear",
+//     quantity: 6,
+//     price: 100,
+//     isFresh: true,
+//   },
+//   {
+//     name: "lemon",
+//     quantity: 12,
+//     price: 60,
+//     isFresh: false,
+//   },
+// ];
+
+// const totalPrice = fruits.reduce(
+//   (total, {price,quantity}) => total + price * quantity,
+//   0
+// );
+
+// console.log(`Загальна вартість: ${totalPrice}`);
+
+// const contacts = [
+//   {
+//     firstName: "Akira",
+//     lastName: "Laine",
+//     number: "0543236543",
+//     likes: ["Pizza", "Coding", "Brownie Points"],
+//     isOnline: true,
+//   },
+//   {
+//     firstName: "Harry",
+//     lastName: "Potter",
+//     number: "0994372684",
+//     likes: ["Hogwarts", "Magic", "Hagrid"],
+//     isOnline: false,
+//   },
+// ];
+
+// // const allLikes = contacts.reduce((likes, contact) => {
+// //   likes.push(...contact.likes);
+// //   return likes;
+// // }, []);
+// // console.log(allLikes);
+
+
+// const allLikes = contacts.reduce((likes, contact) => [...likes,...contact.likes], []);
+// console.log(allLikes);
+
