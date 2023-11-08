79300714ce1fbaf20808cf61f296f21e91e47fce
mari4e4ka <126088784+mari4e4ka@users.noreply.github.com>
2023-11-08 14:36:39 +0100

    update module-5.js

--- module-5.js
+++ module-5.js
@@ -226,24 +226,206 @@
 //       isFresh: false,
 //     },
 //   ],
-// //   countTotalPrice(fruitName) {
-// //     for (const fruit of this.fruits) {
-// //         let totalPrice;
-// //         if(fruit.name===fruitName){
-// //             totalPrice=fruit.price*fruit.quantity;
-// //         return totalPrice;
-// //         }
+//   //   countTotalPrice(fruitName) {
+//   //     for (const fruit of this.fruits) {
+//   //         let totalPrice;
+//   //         if(fruit.name===fruitName){
+//   //             totalPrice=fruit.price*fruit.quantity;
+//   //         return totalPrice;
+//   //         }
 
-// //     }
-// //   },
+//   //     }
+//   //   },
 
-// countTotalPrice(fruitName) {
-//    const item= this.fruits.find(({name})=>name===fruitName);
-//    return item.price*item.quantity;
-// }
+//   // countTotalPrice(fruitName) {
+//   //    const item= this.fruits.find(({name})=>name===fruitName);
+//   //    return item.price*item.quantity;
+//   // }
 
+//   countTotalPrice(fruitName) {
+//     const { quantity, price } = this.fruits.find(
+//       ({ name }) => name === fruitName
+//     );
+//     return price * quantity;
+//   },
 // };
 
 // console.log(fruitStorage.countTotalPrice("apple"));
 // console.log(fruitStorage.countTotalPrice("pear"));
 // console.log(fruitStorage.countTotalPrice("lemon"));
+
+// // const {price, quantity}=fruitStorage.fruits.find((fruit)=>fruit.name==="apple");
+// // const {price, quantity}=fruitStorage.fruits.find(({name})=>name==="lemon");
+
+// // console.log(price);
+// // console.log(quantity);
+
+// const{name, quantity,price,isFresh}=    {
+//     name: "lemon",
+//     quantity: 12,
+//     price: 60,
+//     isFresh: false,
+//   };
+
+//   console.log(name);
+//   console.log(price);
+//   console.log(quantity);
+//   console.log(isFresh);
+
+// const fruitStorage = {
+//   fruits: [
+//     {
+//       name: "apple",
+//       quantity: 10,
+//       price: 10,
+//       isFresh: false,
+//     },
+//     {
+//       name: "pear",
+//       quantity: 6,
+//       price: 100,
+//       isFresh: true,
+//     },
+//     {
+//       name: "lemon",
+//       quantity: 12,
+//       price: 60,
+//       isFresh: false,
+//     },
+//   ],
+
+//   countTotalPrice(fruitName) {
+//     const { quantity, price } = this.fruits.find(
+//       ({ name }) => name === fruitName
+//     );
+//     return `Загальна вартість всіх ${fruitName} на складі ${
+//       price * quantity
+//     } грн`;
+//   },
+// };
+
+// const fruitStorage2 = {
+//   fruits: [
+//     {
+//       name: "banana",
+//       quantity: 100,
+//       price: 30,
+//       isFresh: false,
+//     },
+//     {
+//       name: "peach",
+//       quantity: 12,
+//       price: 80,
+//       isFresh: true,
+//     },
+//     {
+//       name: "cherry",
+//       quantity: 120,
+//       price: 160,
+//       isFresh: false,
+//     },
+//   ],
+// };
+
+// console.log(fruitStorage.countTotalPrice("apple"));
+// console.log(fruitStorage.countTotalPrice("pear"));
+// console.log(fruitStorage.countTotalPrice("lemon"));
+
+// //   console.log(fruitStorage.countTotalPrice.call(fruitStorage2,"banana"));
+// //   console.log(fruitStorage.countTotalPrice.call(fruitStorage2,"peach"));
+// //   console.log(fruitStorage.countTotalPrice.call(fruitStorage2,"cherry"));
+
+// const calcFunction2 = fruitStorage.countTotalPrice.bind(fruitStorage2);
+
+// console.log(calcFunction2("cherry"));
+// console.log(calcFunction2("peach"));
+// console.log(calcFunction2("banana"));
+
+// const calk = {
+//   read(a, b) {
+//     this.a = a;
+//     this.b = b;
+//   },
+//   add() {return `${this.a} + ${this.b} = ${this.a+this.b}`},
+//   mult() {return `${this.a} * ${this.b} = ${this.a*this.b}`},
+// };
+// console.log(calk);
+// calk.read(5,5);
+// console.log(calk);
+// console.log(calk.add());
+// console.log(calk.mult());
+
+// const calk = {
+//     read(a, b) {
+//       this.a = a;
+//       this.b = b;
+//     },
+//     add() {return (this.a??0)+(this.b??0)},
+//     mult() {return (this.a??1)*(this.b??1)},
+//   };
+//   console.log(calk);
+//   calk.read(5,-1);
+//   console.log(calk);
+//   console.log(calk.add());
+//   console.log(calk.mult());
+
+// const objC={c:10,};
+// console.log(objC);
+
+// const objB=Object.create(objC);
+// console.log(objB);
+
+// objB.b=100;
+
+// console.log(objB);
+
+// console.log(`objC.c - `,objC.c);
+// console.log(`objB.b - `,objB.b);
+// console.log(`objB.c - `,objB.c);
+// console.log(`objC.b - `,objC.b);
+
+// const objA=Object.create(objB);
+// console.log(objA);
+
+// objA.a=1000;
+// console.log(objA);
+
+// console.log(objA.hasOwnProperty("b"));
+
+// const Car = function (brand, model, price) {
+//     this.brand=brand;
+//     this.model=model;
+//     this.price=price;
+// };
+// console.log(Car);
+
+// const newCar1 = new Car("audi", "a-6", 43000);
+
+// const newCar2 = new Car("honda", "h-10", 54000);
+
+// console.log(newCar1);
+// console.log(newCar2);
+
+// const Car = function ({price,model,brand}={}) {
+// // const {price,model,brand}=config;
+
+//   this.brand = brand;
+//   this.model = model;
+//   this.price = price;
+// };
+// Car.prototype.changePrice=function(newPrice){
+//     this.price=newPrice;
+// };
+
+// console.log(Car);
+
+// const car1 = new Car({ brand: "audi", model: "a-6", price: 43000 });
+// console.log(car1);
+
+// car1.changePrice(49000);
+// console.log(car1);
+
+
+// const car2 = new Car({ brand: "honda", model: "h-10", price: 54000 });
+// car2.changePrice(58000)
+// console.log(car2);
